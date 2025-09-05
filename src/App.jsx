58df9f69e63947c9cefc13b13d07c8a91454eb69
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-05 07:21:15 +0200

    Add TipButton custom

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,25 @@ import Input from './components/Input'
 import Logo from '../images/logo.svg'
 import IconDollar from '../images/icon-dollar.svg'
 import TipButton from './components/TipButton'
+import TipButtonCustom from './components/TipButtonCustom'
 
 function App() {
 
   return (
     <main>
       <img src={Logo} alt="logo" className='m-auto py-[50px]'/>
-      <section id='calculator' className='h-screen flex flex-col items-center px-6 py-8 bg-white rounded-t-2xl'>
+      <section id='calculator' className='h-screen flex flex-col items-center gap-8 px-6 py-8 bg-white rounded-t-2xl'>
         <Input name="bill" src={IconDollar} alt="Icon Dollar"/>
-        <section id='tip-selection'>
-          <h2 className='preset-5 text-grey-500'>Select tip %</h2>
-          <TipButton text="5%"/>
+        <section id='tip-selection' className='w-full'>
+          <h2 className='preset-5 text-grey-500 pb-2'>Select tip %</h2>
+          <div className='grid grid-cols-2 gap-2'>
+            <TipButton text="5%"/>
+            <TipButton text="10%"/>
+            <TipButton text="15%"/>
+            <TipButton text="25%"/>
+            <TipButton text="50%"/>
+            <TipButtonCustom />
+          </div>
         </section>
       </section>
     </main>
---
+++ src/components/TipButtonCustom.jsx
@@ -0,0 +1,5 @@
+export default function TipButtonCustom() {
+  return (
+    <button className="w-full preset-3 text-grey-550 px-2 py-4 bg-grey-50 rounded">Custom</button>
+  )
+}
