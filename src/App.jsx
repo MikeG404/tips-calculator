d30dae3fdcc79344bfb3e0e1eda05ad7031e9861
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-05 07:01:15 +0200

    Logo position

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
 import Input from './components/Input'
 
+import Logo from '../images/logo.svg'
 import IconDollar from '../images/icon-dollar.svg'
 
 function App() {
 
   return (
     <main>
-      <h1 className='text-center'>SPLI <br></br> TTER</h1>
-      <section id='calculator' className='h-screen flex flex-col items-center bg-white rounded-t-2xl'>
+      <img src={Logo} alt="logo" className='m-auto py-[50px]'/>
+      <section id='calculator' className='h-screen flex flex-col items-center px-6 py-8 bg-white rounded-t-2xl'>
         <Input name="bill" src={IconDollar} alt="Icon Dollar"/>
       </section>
     </main>
