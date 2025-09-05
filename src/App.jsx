cf16f33238709427a16adee75a0f30fcce53566f
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-05 07:13:49 +0200

    Make TipButton

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Input from './components/Input'
 
 import Logo from '../images/logo.svg'
 import IconDollar from '../images/icon-dollar.svg'
+import TipButton from './components/TipButton'
 
 function App() {
 
@@ -11,6 +12,10 @@ function App() {
       <img src={Logo} alt="logo" className='m-auto py-[50px]'/>
       <section id='calculator' className='h-screen flex flex-col items-center px-6 py-8 bg-white rounded-t-2xl'>
         <Input name="bill" src={IconDollar} alt="Icon Dollar"/>
+        <section id='tip-selection'>
+          <h2 className='preset-5 text-grey-500'>Select tip %</h2>
+          <TipButton text="5%"/>
+        </section>
       </section>
     </main>
   )
---
+++ src/components/TipButton.jsx
@@ -0,0 +1,5 @@
+export default function TipButton({text}) {
+  return (
+    <button className="w-full preset-3 text-white px-2 py-4 bg-green2-900 rounded">{text}</button>
+  )
+}
