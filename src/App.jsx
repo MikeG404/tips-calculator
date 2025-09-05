1724a7c65a9de1582e8ade11709db7c26ea89754
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-05 05:59:08 +0200

    Setup CSS config

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="font-mono text-preset-1 text-red-600">
+        Ton texte ici
+      </h1>
+    </>
+  )
+}
+
+export default App
